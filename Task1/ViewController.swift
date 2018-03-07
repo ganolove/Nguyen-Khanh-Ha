2afed9f4a3c1ed80d032b1fa0cb007f18b25988e
User11 <user11@User11noMacBook-Pro.local>
2018-03-07 12:19:21 +0900

    Initial Commit

---
+++ Task1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by User11 on 2018/03/07.
+//  Copyright © 2018年 テストアプリ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
